21c80c9074ac3afcc364e8c2a223a64dec09be0e
Nicolas Bigiotti <nicobigio01@gmail.com>
2024-05-12 00:32:45 +0200

    Work in progress

--- scripts/LNA.m
+++ scripts/LNA.m
@@ -11,7 +11,24 @@ clc
 clear
 close all
 
-%addpath('/home/nicobigio01/cadence_vm/fromCadence/lab_ele1_cadence_ex2');
+% Useful options
+bodeopts_mag = bodeoptions;
+
+bodeopts_mag.FreqUnits = 'Hz';
+bodeopts_mag.PhaseVisible = 'off';
+bodeopts_mag.Grid = 'on';
+
+
+bodeopts_phase = bodeoptions;
+
+bodeopts_phase.FreqUnits = 'Hz';
+bodeopts_phase.MagVisible = 'off';
+bodeopts_phase.Grid = 'on';
+
+
+addpath('/home/nicobigio01/cadence_vm/fromCadence/LabEle2');
+
+currentImgPath = "";
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -19,41 +36,71 @@ close all
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Costants
+k = 13.8e-24; % J/K
+T = 300; % K
+
+
 % OPAMP characteristics
 A_0_db = 40; %dB
 A_0 = db2mag(A_0_db);
 UGBW = 75e6; %Hz
 PM =  80; %degree
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%   OPAMP Transfer function calculation for cadence simulation
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Calculate the first dominant pole of the opamp TF
-tau_PD = A_0 / (2*pi*UGBW);
-
-tau_PS = 1 / (2*pi*((PM - 45) * 0.2 *UGBW + UGBW));
-
-s = tf('s');
-A = A_0 / ((1+s*tau_PD)*(1+s*tau_PS))
-
-%controlSystemDesigner('bode',A);
-bode(A)
-f_highpass = 0.5e6;
-LNA_gain_db =  10;
+% LNA characteristics
+f_hp_LNA = 0.5e6; %Hz
+LNA_gain_db =  10; %dB
 LNA_gain = db2mag(LNA_gain_db);
 
-R_F = 100e3;
-C_F = 1/(f_highpass*R_F)
-C_S = C_F*db2mag(10)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   OPAMP Transfer function evaluation for cadence simulation
+%a
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+% Calculate the first dominant pole of the opamp TF in order to respect
+% UGBW
+tau_A = A_0 / (2*pi*UGBW);
 
-T = A_0*(1+s*R_F*C_F)/(1+s*(C_F+C_S)*(R_F)*(1+s*tau_PD))
-controlSystemDesigner('bode',T);
+% Calculate the second pole from phase margin costraint
+tau_B = tan(-deg2rad(-180+PM)-atan(tau_A*2*pi*UGBW))/(2*pi*UGBW);
+
+% Adding a parametric right hand zero
+tau_C = logspace(8,10,3);
+
+% OPAMP Transfer Function
+s = tf('s');
+A = A_0 / ((1+s*tau_A)*(1+s*tau_B))
+
+%controlSystemDesigner('bode',A);
+
+[A_mag , A_phase , A_w] = bode(A);
+
+h1 = figure();
+semilogx(A_w./(2*pi) , mag2db(reshape( A_mag(1,1,:) , [1 96])));
+hold on
+xline(UGBW , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red")
+yline(0 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red")
+text(UGBW - 50e6, -70 ,  "UGBW: 75 MHz" , "BackgroundColor" , "white" , "EdgeColor" , "red");
+ylabel("Mag [dB]");
+xlabel("Freq log[Hz]");
+title("Modulo Modello OPAMP");
+ylim([-80 45])
+grid on;
+
+h2 = figure();
+semilogx(A_w./(2*pi) , reshape( A_phase(1,1,:) , [1 96]));
+hold on;
+xline(UGBW , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+yline(-180+83.5 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+text(UGBW - 50e6, -150 ,  "PM: 83.5°" , "BackgroundColor" , "white" , "EdgeColor" , "red");
+ylabel("Phase [°]");
+xlabel("Freq [Hz]");
+title("Fase Modello OPAMP");
+grid on;
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -61,7 +108,210 @@ controlSystemDesigner('bode',T);
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Nothing to calculate!
+% VCM si applied at node inp, for virtual ground principle inm is biased at
+% same potential and so the output since the LNA configuration in DC is
+% basically a buffer.
 
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Small Signal
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+RF = logspace(3,5,3);
+
+% By setting the R_F value and knowing that the pole frequency sets the
+% highpass frequency corner we have:
+CF = 1./(2*pi*f_hp_LNA.*RF);
+
+% From the small signal gain at center frequency which is C_S/C_F we
+% obtain:
+CS = CF.*LNA_gain;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Noise
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+f_space = 2*pi*logspace(1,10,200);
+
+% First we set the opamp noise to 5nV/sqrt(Hz)
+v_nopamp = [5e-9 8e-9 10e-9]; %V/sqrt(Hz)
+
+
+
+syms f R_F C_F C_S VNOPAMP
+
+v_nr(R_F) = sqrt(4*k*T*R_F);
+
+A_LNA(f , R_F , C_F , C_S , VNOPAMP) = ((2*pi*f*R_F*C_S)^2)/(1+(2*pi*f*R_F*C_F)^2);
+
+VN_R(f , R_F , C_F , C_S , VNOPAMP) = (v_nr^2)/(1+(2*pi*f*R_F*C_F)^2);
+VN_OPAMP(f , R_F , C_F , C_S , VNOPAMP) =(VNOPAMP^2*((2*pi*f*(C_S+C_F)*R_F)^2+1))/(1+(2*pi*f*R_F*C_F)^2);
+VNIN_OPAMP(f , R_F , C_F , C_S , VNOPAMP) = VN_OPAMP / A_LNA;
+VNIN_R(f , R_F , C_F , C_S , VNOPAMP) = VN_R / A_LNA;
+
+VN(f , R_F , C_F , C_S , VNOPAMP) = VN_R+VN_OPAMP;
+VNIN(f , R_F , C_F , C_S , VNOPAMP) = VN*(1+(2*pi*f*R_F*C_F)^2)/((2*pi*f*R_F*C_S)^2);
+
+
+noise = [sqrt(double(VNIN(f_space,RF(1),CF(1),CS(1),v_nopamp(1)))) ; sqrt(double(VNIN(f_space,RF(2),CF(2),CS(2),v_nopamp(1)))) ; sqrt(double(VNIN(f_space,RF(3),CF(3),CS(3),v_nopamp(1))))];
+
+h3 = figure();
+
+loglog(f_space , noise(1,:));
+grid on;
+hold on;
+loglog(f_space , noise(2,:));
+loglog(f_space , noise(3,:));
+title("R_F Contribution to input referred noise")
+xlabel("f log[Hz]")
+ylabel("v_{nin} log[V/sqrt(Hz)]")
+legend("R_F=1kOhm" , "R_F=10kOhm" , "R_F=100kOhm")
+
+noise = [sqrt(double(VNIN(f_space,RF(3),CF(3),CS(3),v_nopamp(1)))) ; sqrt(double(VNIN(f_space,RF(3),CF(3),CS(3),v_nopamp(2)))) ; sqrt(double(VNIN(f_space,RF(3),CF(3),CS(3),v_nopamp(3))))];
+
+
+h4 = figure();
+
+loglog(f_space , noise(1,:));
+grid on;
+hold on;
+loglog(f_space , noise(2,:));
+loglog(f_space , noise(3,:));
+title("Miller OTA Contribution to input referred noise")
+xlabel("f log[Hz]")
+ylabel("v_{nin} log[V/sqrt(Hz)]")
+legend("v_{n,OTA}=5nV/sqrt(Hz)" , "v_{n,OTA}=8nV/sqrt(Hz)" , "v_{n,OTA}=10nV/sqrt(Hz)")
+
+
+
+
+
+
+
+GLoop = -A*(1+s*RF(3)*CF(3))/(1+s*RF(3)*(CF(3)+CS(3)));
+[GLoop_mag , GLoop_phase , GLoop_w] = bode(GLoop);
+
+h5 = figure();
+semilogx(GLoop_w./(2*pi) , mag2db(reshape( GLoop_mag(1,1,:) , [1 110])));
+hold on
+xline(18.9E6 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red")
+yline(0 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red")
+text(18.9E6, -70 ,  "f: 18.9 MHz" , "BackgroundColor" , "white" , "EdgeColor" , "red");
+ylabel("Mag [dB]");
+xlabel("Freq log[Hz]");
+title("G_{LOOP} - R_F=100kOhm, C_S=3pF, C_F=11pF");
+ylim([-80 45])
+grid on;
+
+h6 = figure();
+semilogx(GLoop_w./(2*pi) , reshape( GLoop_phase(1,1,:) , [1 110]));
+hold on;
+xline(18.9E6 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+yline(88.91 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+text(UGBW - 50e6, -150 ,  "PM: 88.4°" , "BackgroundColor" , "white" , "EdgeColor" , "red");
+ylabel("Phase [°]");
+xlabel("Freq [Hz]");
+title("Fase Modello OPAMP");
+grid on;
+
+
+GLoop = -A*(1-s*6.28e-9)*(1+s*RF(3)*CF(3))/(1+s*RF(3)*(CF(3)+CS(3)));
+[GLoop_mag , GLoop_phase , GLoop_w] = bode(GLoop);
+
+h7 = figure();
+semilogx(GLoop_w./(2*pi) , mag2db(reshape( GLoop_mag(1,1,:) , [1 110])));
+hold on
+xline(18.9E6 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red")
+yline(0 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red")
+text(18.9E6, -70 ,  "f: 18.9 MHz" , "BackgroundColor" , "white" , "EdgeColor" , "red");
+ylabel("Mag [dB]");
+xlabel("Freq log[Hz]");
+title("G_{LOOP} - R_F=100kOhm, C_S=3pF, C_F=11pF");
+ylim([-80 45])
+grid on;
+
+h8 = figure();
+semilogx(GLoop_w./(2*pi) , reshape( GLoop_phase(1,1,:) , [1 110]));
+hold on;
+xline(18.9E6 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+yline(51.55 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+text(UGBW - 50e6, -150 ,  "PM: 88.4°" , "BackgroundColor" , "white" , "EdgeColor" , "red");
+ylabel("Phase [°]");
+xlabel("Freq [Hz]");
+title("Fase Modello OPAMP");
+grid on;
+
+%controlSystemDesigner('bode',GLoop);
+
+
+%T = A_0*(1+s*R_F*C_F)/(1+s*(C_F+C_S)*(R_F)*(1+s*tau_A))
+%%controlSystemDesigner('bode',T);
+%
+%tmp = importdata("LabEle2_SystemDesignTest_FrequencyResponse.matlab");
+%cadence_FreqRes_f = tmp.data(:,1);
+%cadence_FreqRes_A = tmp.data(:,2);
+%tmp = importdata("LabEle2_SystemDesignTest_SineOut.matlab");
+%cadence_SineOut_t = tmp.data(:,1);
+%cadence_SineOut_V = tmp.data(:,2);
+%tmp = importdata("LabEle2_SystemDesignTest_SineIn.matlab");
+%cadence_SineIn_t = tmp.data(:,1);
+%cadence_SineIn_V = tmp.data(:,2);
+%
+%
+%% Find 3dB frequency
+%[Max_gain,index1]=max(cadence_FreqRes_A);
+%freq_max=cadence_FreqRes_f(index1);
+%Gain_3db=Max_gain-3;
+%idxr = find(diff(sign(cadence_FreqRes_A-Gain_3db)))+(-1:1);
+%
+%for k = 1:size(idxr,1)
+%    f_3db(k) = interp1(cadence_FreqRes_A(idxr(k,:)),cadence_FreqRes_f(idxr(k,:)),Gain_3db,'spline');
+%end
+%
+%
+%h3 = figure();
+%
+%semilogx(cadence_FreqRes_f , cadence_FreqRes_A)
+%hold on;
+%yline(max(cadence_FreqRes_A)-3 , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+%xline(f_3db(1) , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+%xline(f_3db(2) , "LineWidth" , 1.5 , "LineStyle" , "--" , "Color" , "red");
+%text(f_3db(1) - 300e3, -30 ,  sprintf("f high pass: %.1f kHz" , f_3db(1)/1e3) , "BackgroundColor" , "white" , "EdgeColor" , "red");
+%text(f_3db(2) -10e6, -30 ,  sprintf("f low pass: %.1f MHz" , f_3db(2)/1e6) , "BackgroundColor" , "white" , "EdgeColor" , "red");
+%grid on;
+%legend("LNA Frequency Response");
+%title("LNA Frequency Response");
+%ylabel("Mag [dB]");
+%xlabel("Freq [Hz]");
+%ylim([-35 15]);
+%
+%h4 = figure();
+%plot(cadence_SineOut_t,cadence_SineOut_V);
+%hold on;
+%plot(cadence_SineIn_t,cadence_SineIn_V);
+%grid on;
+%legend("v_{out}" , "v_{in}");
+%title("LNA Transient Analysis");
+%ylabel("Voltage [V]");
+%xlabel("Time [s]");
+%xlim([0 10e-6]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Results summary
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fprintf('*** OPAMP Model Summary ***\n');
+fprintf('tau_A: %f\n' , tau_A);
+fprintf('tau_A: %f\n' , tau_B);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -70,8 +320,7 @@ controlSystemDesigner('bode',T);
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%saveas(f1,"../latex/img/lab_ele1_pract_ex3_static.png");
-%saveas(f2,"../latex/img/lab_ele1_pract_ex3_mod_inv.png");
-%saveas(f3,"../latex/img/lab_ele1_pract_ex3_phase_inv.png");
-%saveas(f4,"../latex/img/lab_ele1_pract_ex3_mod_rlc.png");
-%saveas(f5,"../latex/img/lab_ele1_pract_ex3_phase_rlc.png");
+% saveas(h1,"../latex/design_review1_26032024/img/LabEle2_LNA_opamp_model_tf_mag.png");
+% saveas(h2,"../latex/design_review1_26032024/img/LabEle2_LNA_opamp_model_tf_phase.png");
+saveas(h3,"../latex/design_review1_26032024/img/LabEle2_LNA_opamp_model_VNIN_R_contr.png");
+saveas(h4,"../latex/design_review1_26032024/img/LabEle2_LNA_opamp_model_VNIN_OTA_contr.png");
--- scripts/miller_opamp.m
+++ scripts/miller_opamp.m
@@ -31,79 +31,239 @@ mu_n = 0.0348; % m^2/(V*s)
 V_builtin = 0.6; % V
 V_TH = 0.55; % V
 VDD = 1.8; %V
-lambda_prime = 5e-8;
-
-
+lambda_prime_n = 5e-8;  % Must fix these two values
+lambda_prime_p = 5e-8;  
 K_n = 0.5 * mu_n * epsilon_ox / t_ox;
-%K_p = 35.6e-6;
 K_p = K_n / 3;
 C_ox = epsilon_ox / t_ox;
-C_L = -1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%   Design Parameter
+%   Design Specs
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+f_hi = 500e3; %Hz
+f_lo = 25e6; %Hz
+LNA_gain_db =  10; %dB
+LNA_gain = db2mag(LNA_gain_db);
+
+%%% Miller Opamp gain, phase margin and UGBW
+A_0_db = 40; %dB
+A_0 = db2mag(A_0_db);
+UGBW = f_lo * LNA_gain; %Hz    % 75MHz from design specs, needs to be a little higher
+                    % to accomodate the 25MHz low pass frequency of the full LNA
+PM = 85; %deg   % 80 deg from specs
+
+% Load capacitor and resistance
+% These values are just assumptions
+C_L = 1e-12; %F
+R_L = 1e9; %Ohm
+
+C_F = C_L;
+
+R_F = 1./(2*pi*f_hi.*C_F);
+
+% From the small signal gain at center frequency which is C_S/C_F we
+% obtain:
+C_S = C_F.*LNA_gain;
+
+%Input referred noise spec
+VIRN = 5e-9; %V/sqrt(Hz)
+
+% Poles estimation from two dominant pole model of opamp
+tau_A = A_0 / (2*pi*UGBW);
+tau_B = tan(-deg2rad(-180+PM)-atan(tau_A*2*pi*UGBW))/(2*pi*UGBW);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Design Procedure
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Defining dummy variables
 % DCOP
-V_OV1 = -1; V_OV2 = -1; V_OV3 = -1; V_OV4 = -1; V_OV6 = -1;
-I_1 = -1; I_2 = -1; I_3 = -1; I_4 = -1; I_6 = -1;
-gm1 = -1; gm2 = -1; gm3 = -1; gm4 = -1; gm6 = -1;
-rds1 = -1; rds2 = -1; rds3 = -1; rds4 = -1; rds6 = -1;
+V_OV1 = -1; V_OV2 = -1; V_OV3 = -1; V_OV4 = -1; V_OV5 = -1; V_OV6 = -1; V_OV7 = -1;
+I1 = -1; I2 = -1; I3 = -1; I4 = -1; I5 = -1; I6 = -1; I7 = -1; I8 = -1;
+gm1 = -1; gm2 = -1; gm3 = -1; gm4 = -1; gm5 = -1; gm6 = -1; gm7 = -1; gm8 = -1;
+rds1 = -1; rds2 = -1; rds3 = -1; rds4 = -1; rds5 = -1; rds6 = -1; rds7 = -1; rds8 = -1;
 
 % MOS Sizing
-W1 = -1; W2 = -1; W3 = -1; W4 = -1; W6 = -1; 
-L1 = -1; L2 = -1; L3 = -1; L4 = -1; L6 = -1; 
+W1 = -1; W2 = -1; W3 = -1; W4 = -1; W5 = -1; W6 = -1; W7 = -1; W8 = -1;
+L1 = 500e-9; L2 = 500e-9; L3 = 500e-9; L4 = 500e-9; L5 = 1500e-9; L6 = 500e-9; L7 = 1500e-9; L8 = 1500e-9;
 
 % Compensation 
 C_C = -1; R_C = -1;
 
 % Total Bias Current
-I_A = -1; I_B = -1;
+IA = -1; IB = -1;
 
 
+%%% Design
+
+IREF = 100e-6; % Fix a reasonable value for the reference current
+
+% Keep C_C 2-4 times greater than C_L to comply to assumptions
+C_C = 3*C_L;
+
+% Evaluate gm1 constraint from bandwidth and noise requirements
+gm1_noise = (16*k*T)/(3*VIRN^2); % gm1 needed for noise constraint 
+gm1_bw = 2 * pi * UGBW * C_C; % gm1 needed for bandwidth constraint 
+
+gm1 = gm1_bw*1.30; % Boost gm a little
+gm2 = gm1;
+
+% Fix overdrive of M1, lower means less current but also larger devices
+V_OV1 = 0.15; %V
+V_OV2 = V_OV1;
+
+% Fix first stage currents
+I1 = 0.5 * V_OV1 * gm1;
+I2 = I1;
+I3 = I1;
+I4 = I1;
+IA = 2 * I1;
+I5 = IA;
+
+%%% Second Stage
+
+% Evaluate gm from phase margin costraint
+gm6 = 1.3 * C_L / tau_B;
+
+% Fix M6 overdrive remebering that is the same of M3 and M4
+V_OV6 = 0.2;
+V_OV3 = V_OV6;
+V_OV4 = V_OV6;
+
+% Calculate all currents
+I6 = 0.5 * V_OV6 * gm6;
+IB = I6;
+I7 = IB;
+
+% Fix all current mirror MOS overdrives
+V_OV5 = 0.25;
+V_OV7 = 0.25;
+V_OV8 = 0.25;
+I8 = IREF;
 
 
-A_v_db = 40; %dB
-A_v = db2mag(A_v_db);
-UGBW = 75e6; %Hz
-PM = 80; %degree
-PSD = 10e-9; %V/sqrt(HZ)
+% Evaluate all Ws
+W1 = (I1*L1) / ( K_n * V_OV1^2 );
+W2 = (I2*L2) / ( K_n * V_OV2^2 );
+W3 = (I3*L3) / ( K_p * V_OV3^2 );
+W4 = (I4*L4) / ( K_p * V_OV4^2 );
+W5 = (I5*L5) / ( K_n * V_OV5^2 );
+W6 = (I6*L6) / ( K_p * V_OV6^2 );
+W7 = (I7*L7) / ( K_n * V_OV7^2 );
+W8 = (I8*L8) / ( K_n * V_OV8^2 );
 
-%SR=10e7; %V/s
-
-A_1 = 10;
-A_2 = 10;
-
-
-V_OV1 = 0.4;
-
-gm1 = (32*k*T)/(3*PSD*PSD)
-
-I_1 = 0.5 * V_OV1 * gm1
-
-I_2 = I_1;
-I_3 = I_1;
-I_4 = I_1;
-
-I_A = 2 * I_1;
-
-L1 = 2 * lambda_prime * I_1 * A_1 / gm1
+% Evaluate all rds'
+rds1 = (L1)/(lambda_prime_n * I1);
+rds2 = (L2)/(lambda_prime_n * I2);
+rds3 = (L3)/(lambda_prime_n * I3);
+rds4 = (L4)/(lambda_prime_n * I4);
+rds5 = (L5)/(lambda_prime_n * I5);
+rds6 = (L6)/(lambda_prime_n * I6);
+rds7 = (L7)/(lambda_prime_n * I7);
+rds8 = (L8)/(lambda_prime_n * I8);
 
 
+% Evaluate the input referred noise of the first stage without approximations
+VIRN_tot =  sqrt((16*k*T)/(3*(gm1+gm6)));
 
-% L_12 = A_1 *lambda_prime * 0.2
-% W_12 = I_A*L_12 / (K_n*V_OV_12*V_OV_12)
-% 
-% L_34 = L_12;
-% W_34 = K_n / K_p * W_12
-% 
-% 
-% W_56 = 2*I_A*L_12 / (K_p*V_OV_12*V_OV_12)
-% 
-% 
-% I_B = 0.5* I_A * W_56 * L_34 / (W_34*L_34)
+% Evaluate the value of the zero nulling resistor
+R_C=1/gm6;
+
+%Evaluate gain
+A_expected = (gm1*rds1*rds2*gm6*rds6*rds7)/((rds1+rds2)*(rds6+rds7));
+A_expected_db = mag2db(A_expected);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Resume
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+fprintf("Final Resume\nC_F: %.1f pF\nC_S: %.1f pF\nR_F: %.1f kOhm\nExpected Gain: %.1f dB\n\n" ,C_F*1e12 , C_S*1e12 , R_F/1e3);
+
+fprintf("Final Resume\nVNIN_tot: %.1f nV/sqrt(Hz)\nC_C: %.1f pF\nR_C: %.1f Ohm\nExpected Gain: %.1f dB\n\n" , VIRN_tot*1e9 , C_C*1e12 , R_C , A_expected_db);
+
+
+fprintf("M1 sizing resume:\nI1 = %.1f uA\nV_OV1 = %.2f V\nW1 = %.1f um\nL1 = %.1f nm\ngm1 = %.1f mA/V\nrds1 = %.1f kOhm\n\n\n" , ...
+    I1*1e6 , ...
+    V_OV1 , ...
+    W1*1e6 ,...
+    L1*1e9 , ...
+    gm1*1e3 , ...
+    rds1*1e-3 ...
+    );
+
+fprintf("M2 sizing resume:\nI2 = %.1f uA\nV_OV2 = %.2f V\nW2 = %.1f um\nL2 = %.1f nm\ngm2 = %.1f mA/V\nrds2 = %.1f kOhm\n\n\n" , ...
+    I2*1e6 , ...
+    V_OV2 , ...
+    W2*1e6 ,...
+    L2*1e9 , ...
+    gm2*1e3 , ...
+    rds2*1e-3 ...
+    );
+
+fprintf("M3 sizing resume:\nI3 = %.1f uA\nV_OV3 = %.2f V\nW3 = %.1f um\nL3 = %.1f nm\ngm3 = %.1f mA/V\nrds3 = %.1f kOhm\n\n\n" , ...
+    I3*1e6 , ...
+    V_OV3 , ...
+    W3*1e6 ,...
+    L3*1e9 , ...
+    gm3*1e3 , ...
+    rds3*1e-3 ...
+    );
+
+
+    
+
+fprintf("M4 sizing resume:\nI4 = %.1f uA\nV_OV4 = %.2f V\nW4 = %.1f um\nL4 = %.1f nm\ngm2 = %.1f mA/V\nrds4 = %.1f kOhm\n\n\n" , ...
+    I4*1e6 , ...
+    V_OV4 , ...
+    W4*1e6 ,...
+    L4*1e9 , ...
+    gm4*1e3 , ...
+    rds4*1e-3 ...
+    );
+
+fprintf("M5 sizing resume:\nI5 = %.1f uA\nV_OV5 = %.2f V\nW5 = %.1f um\nL5 = %.1f nm\ngm5 = %.1f mA/V\nrds5 = %.1f kOhm\n\n\n" , ...
+    I5*1e6 , ...
+    V_OV5 , ...
+    W5*1e6 ,...
+    L5*1e9 , ...
+    gm5*1e3 , ...
+    rds5*1e-3 ...
+    );
+
+fprintf("M6 sizing resume:\nI6 = %.1f uA\nV_OV6 = %.2f V\nW6 = %.1f um\nL6 = %.1f nm\ngm6 = %.1f mA/V\nrds6 = %.1f kOhm\n\n\n" , ...
+    I6*1e6 , ...
+    V_OV6 , ...
+    W6*1e6 ,...
+    L6*1e9 , ...
+    gm6*1e3 , ...
+    rds6*1e-3 ...
+    );
+
+fprintf("M7 sizing resume:\nI7 = %.1f uA\nV_OV7 = %.2f V\nW7 = %.1f um\nL7 = %.1f nm\ngm7 = %.1f mA/V\nrds7 = %.1f kOhm\n\n\n" , ...
+    I7*1e6 , ...
+    V_OV7 , ...
+    W7*1e6 ,...
+    L7*1e9 , ...
+    gm7*1e3 , ...
+    rds7*1e-3 ...
+    );
+
+fprintf("M8 sizing resume:\nI8 = %.1f uA\nV_OV8 = %.2f V\nW8 = %.1f um\nL8 = %.1f nm\ngm8 = %.1f mA/V\nrds8 = %.1f kOhm\n\n\n" , ...
+    I8*1e6 , ...
+    V_OV8 , ...
+    W8*1e6 ,...
+    L8*1e9 , ...
+    gm8*1e3 , ...
+    rds8*1e-3 ...
+    );
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
